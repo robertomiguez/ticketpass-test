5576bae47ffdafd183391f5ad67eeab5bdef1d2b
miguez <miguezroberto@gmail.com>
2024-09-01 10:14:17 +0100

    add EventCard component Fixes #2

--- src/pages/EventsList.vue
+++ src/pages/EventsList.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>{{ events }}</div>
+  <EventCard
+    v-for="event in events"
+    :key="event.id"
+    :event="event"
+    class="p-1"
+  />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import { getEvents } from '@/services/EventService';
 import type { Event as EventType } from '@/types/Event';
+import EventCard from '@/components/EventCard.vue';
 
 const events = ref<EventType[]>([]);
 const fetchEvents = async () => {
